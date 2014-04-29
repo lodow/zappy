1bbf85405d88834a197533d00c97f97c39c2ee47
maxime94 <maxime.l@live.fr>
2014-04-29 16:56:32 +0200

    ia auth to serveur

--- ia/ia.php
+++ ia/ia.php
@@ -1,9 +1,31 @@
 #!/usr/bin/php
 <?php
 
+function my_exit($fp)
+{
+	fclose($fp);
+	exit(0);
+}
+
 function ia($fp, $team, $host, $port)
 {
 
+	if (fgets($fp) != "BIENVENUE\n")
+	   my_exit($fp);
+	fwrite($fp, $team."\n");
+	$num_client = (int) fgets($fp);
+	$map_size = fgets($fp);
+	$map_size_x = explode(" ", $map_size);
+	$map_size_y =(int) $map_size_x[1];
+	$map_size_x = (int) $map_size_x[0];
+	echo "CLIENT #$num_client connect\nMap $map_size_x x $map_size_y\n";
+	for ($i = 0; $i < 5; $i++)
+	{
+		fwrite($fp, "avance"."\n");
+		if (fgets($fp) != "ok\n")
+	   	   my_exit($fp);
+		sleep(1);
+	}
 }
 
 if ($argc < 5)
@@ -30,7 +52,7 @@ if ($team == "" || $port == -1)
 	echo "USAGE ./$argv[0] -n team_name -p port [-h host]\n";
 	exit(0);
    }
-$fp = fsockopen("udp://".$host, $port, $errno, $errstr);
+$fp = fsockopen($host, $port, $errno, $errstr);
 if (!$fp)
    {
 	echo "ERROR: $errno - $errstr<br />\n";
@@ -39,5 +61,6 @@ if (!$fp)
 else
    {
 	ia($fp, $team, $host, $port);
+	fclose($fp);
    }
 ?>
