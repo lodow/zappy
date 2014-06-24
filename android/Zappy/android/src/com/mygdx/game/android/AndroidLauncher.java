b03294eadf7eabf4f6e233445c9f812a6c8ae98d
maxime94 <maxime.l@live.fr>
2014-06-25 00:01:48 +0200

    add basic connection to server

--- android/Zappy/android/src/com/mygdx/game/android/AndroidLauncher.java
+++ android/Zappy/android/src/com/mygdx/game/android/AndroidLauncher.java
@@ -1,16 +1,29 @@
 package com.mygdx.game.android;
 
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.os.StrictMode.ThreadPolicy;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.mygdx.game.MyGdxGame;
 
+import java.io.IOException;
+
+import manage.map.Network;
+
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+        ThreadPolicy policy = new ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        try {
+            new Network("37.187.16.16", 4242);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		initialize(new MyGdxGame(), config);
 	}
 }
