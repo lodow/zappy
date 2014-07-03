a3260c653ed3fb53640c5366eac6a38b2c1e0d8f
Jean Gravier <jean.gravier@gmail.com>
2014-07-03 11:40:26 +0200

    Added Camera

--- gui/inc/GameEngine.hpp
+++ gui/inc/GameEngine.hpp
@@ -1,22 +1,29 @@
+
 #ifndef GAMEENGINE_HPP_
 # define GAMEENGINE_HPP_
 
+
+# include <glm/glm.hpp>
+# include <glm/gtx/transform.hpp>
 # include <SFML/Graphics.hpp>
 # include <SFML/Window.hpp>
 # include <SFML/OpenGL.hpp>
 # include <list>
 # include <string>
 # include "utils.hpp"
+# include "Model.hpp"
 # include "FontText.hpp"
+# include "Camera.hpp"
 
 extern "C"
 {
-  #include "ring_buffer.h"
-  #include "select.h"
-  #include "network.h"
-  #include "liste.h"
+#include "ring_buffer.h"
+#include "select.h"
+#include "network.h"
+#include "liste.h"
 }
 
+# define UNUSED         __attribute__ ((__unused__))
 # define WINDOW_NAME	"ZAPPY"
 # define REFRESH_RATE	(1 / 60) * 1000
 
@@ -27,19 +34,23 @@ typedef std::list<IEntity *> Map;
 
 class GameEngine {
 private:
-  sf::RenderWindow _window;
-  sf::Font _font;
-  TextList _texts;
-  Map _map;
-  t_net *_client;
-  t_list *_elem;
-  struct timeval _tv;
-  Parser *_parser;
-
+    Camera            _camera;
+    
+    sf::RenderWindow  _window;
+    sf::Font          _font;
+    
+    TextList          _texts;
+    Map               _map;
+    t_net             *_client;
+    t_list            *_elem;
+    struct timeval    _tv;
+    Parser            *_parser;
+    
 public:
-  GameEngine(const int&, const int&);
-  ~GameEngine();
-  void	run();
+    GameEngine(const int&, const int&);
+    ~GameEngine();
+    
+    void	run();
 };
 
 int handle_server(t_selfd *fd, void *serv);
--- gui/src/GameEngine.cpp
+++ gui/src/GameEngine.cpp
@@ -1,115 +1,140 @@
+
 #include "GameEngine.hpp"
 
 int             read_from_server(t_selfd *fd)
 {
-  char          buff[BUFSIZ];
-  int           r;
-  size_t        size;
-
-  r = 0;
-  size = ring_buffer_left_write(fd->rbuff);
-  size = (size < sizeof(buff)) ? size : sizeof(buff);
-  if (size && ((r = read(fd->fd, buff, size)) > 0))
-    write_buffer(fd->rbuff, buff, r);
-  return (r);
+    char        buff[BUFSIZ];
+    int         r;
+    size_t      size;
+    
+    r = 0;
+    size = ring_buffer_left_write(fd->rbuff);
+    size = (size < sizeof(buff)) ? size : sizeof(buff);
+    if (size && ((r = read(fd->fd, buff, size)) > 0))
+        write_buffer(fd->rbuff, buff, r);
+    return (r);
 }
 
-std::string                    get_command(t_selfd *fd)
+std::string               get_command(t_selfd *fd)
 {
-  char                  *cmd;
-  size_t                size;
-  char                  buff[512];
-
-  size = read_buffer(fd->rbuff, buff, sizeof(buff));
-  if (size && ((cmd = static_cast<char *>(memchr(buff, '\n', size))))) {
-      rollback_read_buffer(fd->rbuff, size - (cmd - buff + 1));
-      buff[(cmd - buff)] = '\0';
-      return (std::string(buff));
-  }
-  return (std::string(""));
+    char                  *cmd;
+    size_t                size;
+    char                  buff[512];
+    
+    size = read_buffer(fd->rbuff, buff, sizeof(buff));
+    if (size && ((cmd = static_cast<char *>(memchr(buff, '\n', size))))) {
+        rollback_read_buffer(fd->rbuff, size - (cmd - buff + 1));
+        buff[(cmd - buff)] = '\0';
+        return (std::string(buff));
+    }
+    return (std::string(""));
 }
 
 GameEngine::GameEngine(const int &x, const int &y) :
-  _window(sf::VideoMode(x, y), WINDOW_NAME) {
-
-  /* Init connexion */
-  _client = create_connection("::1", "4242", SOCK_STREAM, &connect_nb);
-  if (!_client)
-    return ;
-  int status;
-  std::cout << "Connecting . ";
-  while ((status = is_connected(_client)) == 1) {
-      usleep(500);
-      std::cout << ".";
-  }
-  if (status == -1) {
-      std::cerr << "Error on connection" << std::endl;
-      return ;
-  }
-  _elem = NULL;
-
-  add_to_list(&_elem, static_cast<void *>(create_fd(_client->socket, NULL, (int (*)())(&handle_server))));
-
-  _tv.tv_sec = 0;
-  _tv.tv_usec = 100000;
-  _parser = new Parser(&_map);
-  do_select(_elem, &_tv, _parser);
-  write(_client->socket, "GRAPHIC\n", 8);
-
-  _map.push_back(new FontText(sf::Vector2i(50, 50), "Bonjour Fabulus!", 12, sf::Color::Black));
-  _map.push_back(new FontText(sf::Vector2i(50, 100), "Bonjour Fabulus!", 12, sf::Color::Black));
-  _parser = new Parser(&_map);
-  this->run();
+_window(sf::VideoMode(x, y), WINDOW_NAME, sf::Style::Default, sf::ContextSettings(32, 0, 3, 3, 0)) {
+    
+    this->run();
+    
+    /* Init connexion */
+    _client = create_connection("::1", "4242", SOCK_STREAM, &connect_nb);
+    if (!_client)
+        return ;
+    int status;
+    std::cout << "Connecting . ";
+    while ((status = is_connected(_client)) == 1) {
+        usleep(500);
+        std::cout << ".";
+    }
+    if (status == -1) {
+        std::cerr << "Error on connection" << std::endl;
+        return ;
+    }
+    _elem = NULL;
+    
+    add_to_list(&_elem, static_cast<void *>(create_fd(_client->socket, NULL, (int (*)())(&handle_server))));
+    
+    _tv.tv_sec = 0;
+    _tv.tv_usec = 100000;
+    _parser = new Parser(&_map);
+    do_select(_elem, &_tv, _parser);
+    write(_client->socket, "GRAPHIC\n", 8);
+    
+    _map.push_back(new FontText(sf::Vector2i(50, 50), "Bonjour Fabulus!", 12, sf::Color::Black));
+    _map.push_back(new FontText(sf::Vector2i(50, 100), "Bonjour Fabulus!", 12, sf::Color::Black));
+    _parser = new Parser(&_map);
 }
 
 GameEngine::~GameEngine() {
-  delete _parser;
+    delete _parser;
 }
 
 void	GameEngine::run() {
-  sf::Texture texture;
-  if (!texture.loadFromFile("res/sprites/zizou.jpg"))
-    return ;
-  sf::Sprite sprite;
-  sprite.setTexture(texture);
+    
+    glEnable(GL_DEPTH_TEST);
+    
+    Shader *shader = new Shader("res/shaders/basic.vert", "res/shaders/basic.frag");
+    Camera camera;
+    Model model;
+    
+    model.loadObj("res/models/barney/barney.obj", true);
+    model.scale(glm::vec3(0.5, 0.5, 0.5));
+    camera.setPos(glm::vec3(0, 1, 2));
+    camera.setPointView(glm::vec3(0, 1, 0));
+    shader->initialiser();
+    
+    while (_window.isOpen()) {
+        sf::Event event;
+        while (_window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed ||
+                event.key.code == sf::Keyboard::Escape)
+                _window.close();
+        }
+        
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        
+        camera.lookAt();
+        shader->bind();
+        shader->setUniform("projection", camera.getProjection());
+        shader->setUniform("view", camera.getTransformation());
+        
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left))
+            camera.translate(glm::vec3(-0.1, 0, 0));
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right))
+            camera.translate(glm::vec3(0.1, 0, 0));
 
-  while (_window.isOpen()) {
-      sf::Event event;
-      while (_window.pollEvent(event)) {
-	if (event.type == sf::Event::Closed ||
-	    event.key.code == sf::Keyboard::Escape)
-	  _window.close();
-      }
-      _window.clear();
-      do_select(_elem, &_tv, _parser);
-      _window.draw(sprite);
-      for (Map::iterator it = _map.begin(), end = _map.end(); it != end; ++it)
-	(*it)->draw(_window);
-      sf::sleep(sf::milliseconds(REFRESH_RATE));
-      _window.display();
+        model.draw(shader);
+        
+//        model.rotate(glm::vec3(0, 1, 0), 0.1);
+        
+        //      do_select(_elem, &_tv, _parser);
+        
+//        for (Map::iterator it = _map.begin(), end = _map.end(); it != end; ++it)
+//            (*it)->draw(_window);
+        sf::sleep(sf::milliseconds(10));
+        _window.display();
     }
 }
 
 int handle_server(t_selfd *fd, void *parser)
 {
-  std::string cmd;
-  int r;
-  ssize_t swr;
-  Parser *cpy = static_cast<Parser *>(parser);
-
-  if (ISREADABLE(fd)) {
-      if (((r = read_from_server(fd)) < 0 && errno != EINTR) || (r == 0))
-	return (-1);
-  }
-//  if (ISWRITEABLE(fd) && (r = write_to_client(fd)) < 0 && errno != EINTR)
-//    return (-1);
-  while ((cmd = get_command(fd)) != "")
-    cpy->parseCmd(cmd);
-  swr = ring_buffer_left_read(fd->wbuff);
-  if (!swr && fd->to_close)
-    return (-1);
-  if (swr)
-    CHECKWRITE(fd);
-  CHECKREAD(fd);
-  return (0);
+    std::string cmd;
+    int r;
+    ssize_t swr;
+    Parser *cpy = static_cast<Parser *>(parser);
+    
+    if (ISREADABLE(fd)) {
+        if (((r = read_from_server(fd)) < 0 && errno != EINTR) || (r == 0))
+            return (-1);
+    }
+    //  if (ISWRITEABLE(fd) && (r = write_to_client(fd)) < 0 && errno != EINTR)
+    //    return (-1);
+    while ((cmd = get_command(fd)) != "")
+        cpy->parseCmd(cmd);
+    swr = ring_buffer_left_read(fd->wbuff);
+    if (!swr && fd->to_close)
+        return (-1);
+    if (swr)
+        CHECKWRITE(fd);
+    CHECKREAD(fd);
+    return (0);
 }
