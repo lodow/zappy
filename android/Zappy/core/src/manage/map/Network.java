d4dba525710344907b751ce1925a66410d22a429
maxime94 <maxime.l@live.fr>
2014-07-03 17:52:52 +0200

    correct bug player (x2)

--- android/Zappy/core/src/manage/map/Network.java
+++ android/Zappy/core/src/manage/map/Network.java
@@ -60,8 +60,8 @@ public class Network {
             parts = tmp.split(" ");
         }
         while (tmp.compareTo("") != 0 && parts[0].compareTo("pnw") == 0) {
-            // players
-            map.addPlayer(new Player(new Vector2(Integer.parseInt(parts[2]), Integer.parseInt(parts[3])), parts[6], Integer.parseInt(parts[1]), Integer.parseInt(parts[5]), Player.eDirection.values()[Integer.parseInt(parts[4])]));
+            // playersl
+            map.addPlayer(new Player(new Vector2(Integer.parseInt(parts[2]), Integer.parseInt(parts[3])), parts[6], Integer.parseInt(parts[1]), Integer.parseInt(parts[5]), Player.eDirection.values()[Integer.parseInt(parts[4]) - 1]));
             if (input.ready())
                 tmp = input.readLine();
             else
@@ -129,7 +129,7 @@ public class Network {
     }
 
     private void pnw() {
-        map.addPlayer(new Player(new Vector2(Integer.parseInt(parts[2]), Integer.parseInt(parts[3])), parts[6], Integer.parseInt(parts[1]), Integer.parseInt(parts[5]), Player.eDirection.values()[Integer.parseInt(parts[4])]));
+        map.addPlayer(new Player(new Vector2(Integer.parseInt(parts[2]), Integer.parseInt(parts[3])), parts[6], Integer.parseInt(parts[1]), Integer.parseInt(parts[5]), Player.eDirection.values()[Integer.parseInt(parts[4]) - 1]));
     }
 
     private void ppo() {
