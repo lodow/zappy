a67ddcf5dbb49f450f23e16f5749fa3bf3eb95eb
debas_e <debas_e@epitech.eu>
2014-07-10 16:01:02 +0200

    solve aff bug team name

--- android/Zappy/core/src/com/zappy/screen/PopUpInformation.java
+++ android/Zappy/core/src/com/zappy/screen/PopUpInformation.java
@@ -30,7 +30,7 @@ public class PopUpInformation extends Stage {
     private boolean showing = false;
     private Player player;
     private HashMap<Square.eType, Image> img_ressource = new HashMap<Square.eType, Image>();
-    private Label level;
+    private Label level, team;
     private HashMap<Square.eType, Label> labelInfo = new HashMap<Square.eType, Label>();
 
     public PopUpInformation (Skin skin) {
@@ -62,13 +62,17 @@ public class PopUpInformation extends Stage {
 
         level = new Label("", skin, "default-font", Color.WHITE);
         level.setFontScale(0.5f);
+        team = new Label("", skin, "default-font", Color.WHITE);
+        team.setFontScale(0.4f);
 
-        table.setSize(Gdx.graphics.getWidth() / 3, Gdx.graphics.getHeight() / 3);
+        table.setSize(Gdx.graphics.getWidth() / 3, Gdx.graphics.getHeight() / 2.8f);
         table.setBackground(patch);
 
         table.columnDefaults(4);
         table.add(level).top().left().expand().colspan(2).padLeft(5);
         table.add(cross).top().right().colspan(2);
+        table.row();
+        table.add(team).top().left().expand().colspan(4).padLeft(5);
         table.row();
 
         img_ressource.get(Square.eType.Thystame).scaleBy(2);
@@ -87,24 +91,26 @@ public class PopUpInformation extends Stage {
         labelInfo.get(Square.eType.Deraumere).setFontScale(0.3f);
         labelInfo.get(Square.eType.Nourriture).setFontScale(0.3f);
 
-        table.add(img_ressource.get(Square.eType.Thystame)).left().bottom().expand().padBottom(15).padLeft(5);
-        table.add(labelInfo.get(Square.eType.Thystame)).left().bottom().expand();
-        table.add(img_ressource.get(Square.eType.Sibur)).left().bottom().expand().padBottom(15);
-        table.add(labelInfo.get(Square.eType.Sibur)).left().bottom().expand();
+        table.add(img_ressource.get(Square.eType.Thystame)).left().bottom().expand().padLeft(5);
+        table.add(labelInfo.get(Square.eType.Thystame)).left().top().expand();
+        table.add(img_ressource.get(Square.eType.Sibur)).left().bottom().expand();
+        table.add(labelInfo.get(Square.eType.Sibur)).left().top().expand();
         table.row();
-        table.add(img_ressource.get(Square.eType.Phiras)).left().bottom().expand().padBottom(15).padLeft(5);
-        table.add(labelInfo.get(Square.eType.Phiras)).left().bottom().expand();
-        table.add(img_ressource.get(Square.eType.Mendiane)).left().bottom().expand().padBottom(15);
-        table.add(labelInfo.get(Square.eType.Mendiane)).left().bottom().expand();
+        table.add(img_ressource.get(Square.eType.Phiras)).left().bottom().expand().padLeft(5);
+        table.add(labelInfo.get(Square.eType.Phiras)).left().top().expand();
+        table.add(img_ressource.get(Square.eType.Mendiane)).left().bottom().expand();
+        table.add(labelInfo.get(Square.eType.Mendiane)).left().top().expand();
         table.row();
-        table.add(img_ressource.get(Square.eType.Linemate)).left().bottom().expand().padBottom(15).padLeft(5);
-        table.add(labelInfo.get(Square.eType.Linemate)).left().bottom().expand();
-        table.add(img_ressource.get(Square.eType.Deraumere)).left().bottom().expand().padBottom(15);
-        table.add(labelInfo.get(Square.eType.Deraumere)).left().bottom().expand();
+        table.add(img_ressource.get(Square.eType.Linemate)).left().bottom().expand().padLeft(5);
+        table.add(labelInfo.get(Square.eType.Linemate)).left().top().expand();
+        table.add(img_ressource.get(Square.eType.Deraumere)).left().bottom().expand();
+        table.add(labelInfo.get(Square.eType.Deraumere)).left().top().expand();
         table.row();
-        table.add(img_ressource.get(Square.eType.Nourriture)).left().bottom().expand().padBottom(15).padLeft(5);
-        table.add(labelInfo.get(Square.eType.Nourriture)).left().bottom().expand();
+        table.add(img_ressource.get(Square.eType.Nourriture)).left().bottom().expand().padLeft(5);
+        table.add(labelInfo.get(Square.eType.Nourriture)).left().top().expand();
         table.setPosition(Gdx.graphics.getWidth() - table.getWidth(), 0);
+
+        //table.debug();
     }
 
     public boolean isShowing() {
@@ -124,7 +130,8 @@ public class PopUpInformation extends Stage {
             Label tmp = entry.getValue();
             tmp.setText(entry.getKey() + " : " + number.toString());
         }
-        level.setText("level : " + player.get_level() + " (team " + player.get_team() + ")");
+        level.setText("level : " + player.get_level());
+        team.setText("(team " + player.get_team() + ")");
     }
 
     private class CrossClick extends ClickListener {
